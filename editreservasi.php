0d64713ac2222ec625b8bde50fb7cc86df3a320c
DevinaCitra <112444724+DevinaCitra@users.noreply.github.com>
2024-06-07 16:11:19 +0700

    Add files via upload

---
+++ editreservasi.php
@@ -0,0 +1,190 @@
+<?php
+  include "Exe-Files/koneksi.php";
+
+  $query_select = mysqli_query($mysqli, "SELECT * FROM `reservasi` WHERE `id` = '$_GET[update]'");
+  $result = mysqli_fetch_assoc($query_select);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ragam Rasa - Edit Reservasi</title>
+    <link rel="icon" href="Assets/img/favicon.ico" sizes="16x16 32x32 48x48 64x64 128x128">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="Assets/css/bootstrap4.min.css" rel="stylesheet">
+    <link href="Assets/css/custome.css" rel="stylesheet">
+    <link href="Assets/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body id="page-top">
+    <div id="wrapper">
+        <ul class="navbar-nav bg-custome sidebar sidebar-dark accordion" id="accordionSidebar">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+                <div class="sidebar-brand-icon">
+                    <img src="Assets/img/brand-logo.png" alt="Logo Brand" width="50px">
+                </div>
+                <div class="sidebar-brand-text mx-2">Ragam Rasa</div>
+            </a>
+
+            <hr class="sidebar-divider my-0">
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <i class="fa-solid fa-fw fa-house-chimney"></i>
+                    <span>Beranda</span>
+                </a>
+            </li>
+
+            <hr class="sidebar-divider">
+
+            <div class="sidebar-heading">Daftar Menu</div>
+
+            <li class="nav-item">
+                <a class="nav-link" href="menu.php">
+                    <i class="fa-solid fa-fw fa-bowl-food"></i>
+                    <span>Menu</span>
+                </a>
+            </li>
+            
+            <li class="nav-item active">
+                <a class="nav-link" href="reservasi.php">
+                    <i class="fa-solid fa-fw fa-list"></i>
+                    <span>Reservasi</span>
+                </a>
+            </li>
+
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+        </ul>
+
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                    <form class="form-inline">
+                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                            <i class="fa-solid fa-bars"></i>
+                        </button>
+                    </form>
+
+                    <a class="nav-link d-flex align-items-center" href="index.php">
+                        <i class="fas fa-fw fa-house-chimney mr-2" style="color: #6e707e"></i>
+                        <h1 class="h4 mb-0 text-gray-700 font-weight-bold">Home</h1>
+                    </a>
+
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                                <img class="img-profile rounded-circle" src="Assets/img/admin-profile.png">
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div class="container-fluid">
+                  <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h5 class="m-0 font-weight-bold text-dark">
+                          Mengubah Data Reservasi
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <div class="form-group w-50">
+                                <label for="nama_pelanggan">Nama Pelanggan <i class="fas fa-star-of-life" style="font-size: 7px; vertical-align: top; color: #ED2939"></i></label>
+                                <input type="text" class="form-control" id="nama_pelanggan" name="nama_pelanggan" value="<?= $result['nama_pelanggan']; ?>" required>
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="nomor_meja">Nomor Meja <i class="fas fa-star-of-life" style="font-size: 7px; vertical-align: top; color: #ED2939"></i></label>
+                                <input type="text" class="form-control" id="nomor_meja" name="nomor_meja" value="<?= $result['nomor_meja']; ?>" required>
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="waktu_reservasi">Waktu Reservasi <i class="fas fa-star-of-life" style="font-size: 7px; vertical-align: top; color: #ED2939"></i></label>
+                                <input type="datetime-local" class="form-control" id="waktu_reservasi" name="waktu_reservasi" value="<?= $result['waktu_reservasi']; ?>" required>
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="jumlah_orang">Jumlah Orang <i class="fas fa-star-of-life" style="font-size: 7px; vertical-align: top; color: #ED2939"></i></label>
+                                <input type="number" class="form-control" id="jumlah_orang" name="jumlah_orang" value="<?= $result['jumlah_orang']; ?>" required>
+                            </div>
+                            <div class="form-group w-50">
+                                <label for="status">Status <i class="fas fa-star-of-life" style="font-size: 7px; vertical-align: top; color: #ED2939"></i></label>
+                                <select class="form-control" id="status" name="status" required>
+                                    <option value="Pending" <?= $result['status'] == 'Pending' ? 'selected' : ''; ?>>Pending</option>
+                                    <option value="Confirmed" <?= $result['status'] == 'Confirmed' ? 'selected' : ''; ?>>Confirmed</option>
+                                    <option value="Cancelled" <?= $result['status'] == 'Cancelled' ? 'selected' : ''; ?>>Cancelled</option>
+                                </select>
+                            </div>
+
+                            <div class="d-sm-flex align-items-center justify-content-start">
+                                <button type="submit" name="btn-simpan" class="btn btn-primary">Simpan</button>
+                                <span class="mr-2"></span>
+                                <a href="reservasi.php" class="btn btn-secondary">Batalkan</a>
+                            </div>
+                        </form>
+
+                        <?php 
+                            if(isset($_POST['btn-simpan'])) {
+                                $id = $result['id'];
+                                $nama_pelanggan = mysqli_real_escape_string($mysqli, $_POST['nama_pelanggan']);
+                                $nomor_meja = mysqli_real_escape_string($mysqli, $_POST['nomor_meja']);
+                                $waktu_reservasi = mysqli_real_escape_string($mysqli, $_POST['waktu_reservasi']);
+                                $jumlah_orang = mysqli_real_escape_string($mysqli, $_POST['jumlah_orang']);
+                                $status = mysqli_real_escape_string($mysqli, $_POST['status']);
+
+                                $query_update = mysqli_query($mysqli, "UPDATE `reservasi` SET `nama_pelanggan`='$nama_pelanggan', `nomor_meja`='$nomor_meja', `waktu_reservasi`='$waktu_reservasi', `jumlah_orang`='$jumlah_orang', `status`='$status' WHERE `id` = '$id'");
+
+                                if($query_update) {
+                                    ?>
+
+                                    <script>
+                                        Swal.fire({
+                                            title: "Berhasil!",
+                                            text: "Data reservasi berhasil diubah!",
+                                            icon: "success"
+                                        }).then(function() {
+                                            window.location.href = 'reservasi.php';
+                                        });
+                                    </script>
+
+                                    <?php
+                                } else {
+                                    echo "Error: " . mysqli_error($mysqli);
+                                }
+                            }
+                        ?>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <footer class="sticky-footer bg-white">
+              <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                  <span>Copyright &copy; Ragam Rasa 2024</span>
+                </div>
+              </div>
+            </footer>
+        </div>
+    </div>
+
+    <script src="Assets/js/jquery.min.js"></script>
+    <script src="Assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="Assets/js/jquery.easing.min.js"></script>
+
+    <script src="Assets/js/sb-admin-2.min.js"></script>
+
+    <script src="Assets/js/jquery.dataTables.min.js"></script>
+    <script src="Assets/js/dataTables.bootstrap4.min.js"></script>
+
+    <script src="Assets/js/datatables-demo.js"></script>
+</body>
+</html>
